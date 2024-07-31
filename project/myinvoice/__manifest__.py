2324c64cd6ca66c4f0f081de040721865f0c5da1
“mayur” <mayurpatel6250@gmail.com>
2024-07-31 13:02:59 +0530

    [add module]

---
+++ project/myinvoice/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'mybill',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'views/invoice_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
